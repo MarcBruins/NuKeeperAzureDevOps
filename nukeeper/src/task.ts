9c7e1ed23e67604e587181d174f5d64779ba805a
Manuel Riezebosch <mriezebosch@gmail.com>
2018-11-16 20:34:34 +0100

    execute repo command

--- nukeeper/src/task.ts
+++ nukeeper/src/task.ts
@@ -1,6 +1,14 @@
 import { execFile } from "mz/child_process";
 import * as path from 'path';
+import * as tl from 'vsts-task-lib';
 
 export async function execNuKeeper(args: string|string[]) {
     return execFile('dotnet', [path.join(__dirname, '..', 'bin', 'NuKeeper.dll')].concat(args));
 }
+
+export async function repo(dir: string, exec: (args: string[]) => Promise<[string, string]> = execNuKeeper) {
+    let token = tl.getEndpointAuthorizationParameter("SystemVssConnection", "AccessToken", false);
+    return exec(['repo', dir, token]);
+}
+
+repo(tl.cwd()).catch(err => tl.setResult(tl.TaskResult.Failed, err));
--- nukeeper/test/task.spec.ts
+++ nukeeper/test/task.spec.ts
@@ -1,11 +1,23 @@
-import { execFile, spawn } from "mz/child_process";
-import * as path from 'path';
 import { expect } from 'chai';
-import { execNuKeeper } from '../src/task';
+import { execNuKeeper, repo } from '../src/task';
+import * as task from '../src/task'
+import * as sinon from 'sinon';
+import * as tl from 'vsts-task-lib';
 
 describe('repo', () => {
+    afterEach(() => {
+        sinon.resetBehavior();
+    });
+
     it ('invokes dotnet with the nukeeper assembly', async () => {
         let result = await execNuKeeper('--version');
         expect(result[0]).to.match(/0\.10\.0/);
+    });
+
+    it ('invoked repo command with uri and access token', async () => {
+        let stub = sinon.stub(tl, 'getEndpointAuthorizationParameter').returns('asdflk;ajsdfl;kjasdf');
+        await repo(__dirname, (_) => { return Promise.resolve([''][''])});
+
+        sinon.assert.called(stub);      
     });
 });
