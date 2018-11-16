c3b8dc5642c68ffba2446c943a8a6c1ac28ff7f0
Manuel Riezebosch <mriezebosch@gmail.com>
2018-11-16 14:16:01 +0100

    install in tmp dir

--- nukeeper/src/task.ts
+++ nukeeper/src/task.ts
@@ -1,8 +1,9 @@
 import { execFile } from "mz/child_process";
-import * as path from 'path';
+import * as tmp from 'tempy';
 
 export async function install(): Promise<string> {
-    await execFile('dotnet', ['tool', 'install', 'nukeeper', '--version', '0.10.0', '--tool-path', __dirname]);
-    return __dirname;
+    let dir = await tmp.directory();
+    await execFile('dotnet', ['tool', 'install', 'nukeeper', '--version', '0.10.0', '--tool-path', dir]);
+    return dir;
 }
 
