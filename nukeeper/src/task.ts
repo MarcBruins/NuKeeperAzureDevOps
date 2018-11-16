4feae3ac2f378a723c9e2bf78f4cd189bebb48a2
Manuel Riezebosch <mriezebosch@gmail.com>
2018-11-16 14:31:02 +0100

    install into cachedir

--- nukeeper/src/task.ts
+++ nukeeper/src/task.ts
@@ -1,9 +1,15 @@
 import { execFile } from "mz/child_process";
 import * as tmp from 'tempy';
+import * as tl from 'vsts-task-tool-lib';
 
+const version = '0.10.0';
 export async function download(): Promise<string> {
     let dir = await tmp.directory();
-    await execFile('dotnet', ['tool', 'install', 'nukeeper', '--version', '0.10.0', '--tool-path', dir]);
+    await execFile('dotnet', ['tool', 'install', 'nukeeper', '--version', version, '--tool-path', dir]);
     return dir;
+}
+
+export async function install(path: string) {
+    return tl.cacheDir(path, 'nukeeper', version);
 }
 
--- nukeeper/test/task.spec.ts
+++ nukeeper/test/task.spec.ts
@@ -1,10 +1,19 @@
 import { expect } from 'chai'
 import * as fs from 'mz/fs';
-import { download } from '../src/task';
+import { download, install } from '../src/task';
+import * as tl from 'vsts-task-tool-lib';
+import sinon = require('sinon');
 
 describe('task', () => {
     it('downloads nukeeper', async () => {
         let location = await download();
         expect(await fs.exists(location)).to.be.true;
     }).timeout(4000);
+
+    it ('moves nukeeper to the tool cache', async () => {
+        let stub = sinon.stub(tl, 'cacheDir').withArgs('asdafasdf', 'nukeeper', '0.10.0');
+        await install('asdafasdf');
+
+        sinon.assert.called(stub);
+    });
 });
