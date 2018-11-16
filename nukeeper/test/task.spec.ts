56514c13bbb8d5f63eb91ce2d9b015ff7e0d605f
Manuel Riezebosch <mriezebosch@gmail.com>
2018-11-16 14:51:59 +0100

    nukeeper binaries included

--- nukeeper/test/task.spec.ts
+++ nukeeper/test/task.spec.ts
@@ -1,19 +1,10 @@
-import { expect } from 'chai'
-import * as fs from 'mz/fs';
-import { download, install } from '../src/task';
-import * as tl from 'vsts-task-tool-lib';
-import sinon = require('sinon');
+import { execFile, spawn } from "mz/child_process";
+import * as path from 'path';
+import { expect } from 'chai';
 
-describe('task', () => {
-    it('downloads nukeeper', async () => {
-        let location = await download();
-        expect(await fs.exists(location)).to.be.true;
-    }).timeout(4000);
-
-    it ('moves nukeeper to the tool cache', async () => {
-        let stub = sinon.stub(tl, 'cacheDir').withArgs('asdafasdf', 'nukeeper', '0.10.0');
-        await install('asdafasdf');
-
-        sinon.assert.called(stub);
+describe('repo', () => {
+    it ('invokes dotnet with the nukeeper assembly', async () => {
+        let result = await execFile('dotnet', [path.join(__dirname, '..', 'bin', 'NuKeeper.dll'), '--version']);
+        expect(result[0]).to.match(/0\.10\.0/);
     });
 });
